63e6053d2cfbbefc19a608351bc49e36573d5298
shiki <kishintorii@gmail.com>
2023-12-28 20:40:09 +0900

    set

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 class scrollHorizontal {
 	constructor() {
 		this.wrapper = '#wrapper';
-		this.panel = '.panel';
+		this.panel = '.contents';
 		this.anchor = '.anchor';
 		this.init();
+		this.anchorScroll();
 	}
 	init() {
 		const wrapper = document.querySelector(this.wrapper);
-		if(wrapper) {
-			// gsap.registerPlugin(ScrollTrigger);
-			const panels = gsap.utils.toArray(this.panel);
+		if(wrapper) {			const panels = gsap.utils.toArray(this.panel);
 			const wrapperWidth = wrapper.offsetWidth;
-			/**
-			 * 横スクロール開始
-			 */
 			gsap.to( panels, {
 				xPercent: -100 * (panels.length - 1),
 				ease: "none",
@@ -26,20 +22,28 @@ class scrollHorizontal {
 			})
 		}
 	}
+	anchorScroll() {
+		const wrapper = document.querySelector(this.wrapper);
+		if(wrapper) {
+			const anchors = document.querySelectorAll(this.anchor);
+			let index = '';
+			anchors.forEach( (anchor) => {
+				anchor.addEventListener( 'click', (e) => {
+					e.preventDefault();
+					index = [].slice.call(anchors).indexOf(anchor);
+					gsap.to(window, {
+						scrollTo: {
+							y: (document.documentElement.scrollHeight - document.documentElement.clientHeight) / 2 * (index + 1),
+							autoKill: false
+						},
+						duration: 1
+					});
+				});
+			});
+		}
+	}
 }
 
 window.addEventListener('load', () => {
 	new scrollHorizontal();
-  console.log(document.body.clientWidth);
-  console.log(document.querySelector(".headerlist > #article > a").getAttribute('href'));
 })
-
-function goTo() {
-  gsap.to(window, {
-    scrollTo: {
-      y: "#panel02",
-      autoKill: false
-    },
-    duration: 1
-  });
-}
