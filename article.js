43e97dd5f898d125988beb26ce5389243a7a4f0f
shiki <kishintorii@gmail.com>
2023-12-29 14:20:48 +0900

    save

--- article.js
+++ article.js
@@ -1,25 +1,41 @@
 fetch('https://raw.githubusercontent.com/shiki-01/misc/main/json/articleList.json')
     .then(result => result.json())
     .then((output) => {
-        console.log('Output: ', output);
 
         var list = output.list;
 
-        console.log(list);
-        list = list.sort(function(a,b){
-            return (a[1] < b[1] ? 1 : -1);
-          });
+        list = list.sort((a, b) => {
+            const aMilliseconds = new Date(a).getTime();
+            const bMilliseconds = new Date(b).getTime();
+            return aMilliseconds - bMilliseconds;
+        });
 
-              console.log(list);
+        var articleList = [];
 
-        list.forEach(function (value) {
-            fetch('https://raw.githubusercontent.com/shiki-01/misc/main/json/' + value + '.json')
-                .then(result => result.json())
-                .then((output) => {
-                    console.log('Output: ', output);
+        async function fetchData() {
 
-                });
-        })
-            .catch(err => console.error(err));
+            for (let i = 0; i < list.length; i++) {
+                try {
+                    var response = await fetch('https://raw.githubusercontent.com/shiki-01/misc/main/json/' + list[i] + '.json');
+                    var data = await response.json();
+                    articleList.push(data);
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+
+        }
+
+        fetchData().then(function () {
+
+            console.log(articleList);
+
+            for (let i = 0; i < 4; i++) {
+                const element = document.querySelector("#newWrap" + (i + 1));
+                console.log('<span class="newImage">' + articleList[i].mainImage + '</span><span class="newTitle">' + articleList[i].title + '</span>' , element)
+                const createElement = '<span class="newImage">' + articleList[i].mainImage + '</span><span class="newTitle">' + articleList[i].title + '</span>';
+                document.getElementById(document.querySelector("#newWrap" + (i + 1))).innerHTML = '<span class="newImage">' + articleList[i].mainImage + '</span><span class="newTitle">' + articleList[i].title + '</span>';
+            }
+        });
     })
-    .catch (err => console.error(err));
+    .catch(err => console.error(err));
