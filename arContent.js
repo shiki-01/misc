09f82a84ed011b2de3ff457372f583ae68ada2f1
shiki <kishintorii@gmail.com>
2023-12-31 11:58:39 +0900

    save

---
+++ arContent.js
@@ -0,0 +1,2 @@
+const value = sessionStorage.getItem('value');
+console.log('受け取った値:', value);
--- article.js
+++ article.js
@@ -30,17 +30,56 @@ fetch('https://raw.githubusercontent.com/shiki-01/misc/main/json/articleList.jso
 
             console.log(articleList);
 
-            for (let i = 0; i < 4; i++) {
-                const element = document.querySelector("#newWrap" + (i + 1));
-                const createElement = '<span class="imageWrap"><img class="newImage" src="' + articleList[i].mainImage + '" alt="mainImage"></span><span class="newTitle">' + articleList[i].title + '</span><span class="newTags"></span><span class="newBody">' + articleList[i].content.substring(0,80) + '…</span>';
-                element.innerHTML = createElement;
+            let num = 0;
 
-                for (let ii = 0; ii < articleList[i].tag.length; ii++) {
-                    const elementTag = document.querySelector("#newWrap" + (i + 1) + " > .newTags");
-                    const createElementTag = '<span class="tag">#' + articleList[i].tag[ii] + '<span>';
-                    elementTag.insertAdjacentHTML("beforeend" , createElementTag);
+            console.log(num);
+
+            for (let i = 0; i < list.length; i++) {
+                //const element = document.querySelector(".allWrapping");
+                //const createElement = '<span class="imageWrap"><img class="newImage" src="' + articleList[num].mainImage + '" alt="' + list[num] + '"></span><span class="newTitle">' + articleList[num].title + '</span><span class="newTags"></span><span class="newBody">' + articleList[num].content.substring(0, 80) + '…</span>';
+                //element.innerHTML = createElement;
+
+                for (let ii = 0; ii < 3; ii++) {
+                    const createElement = '<div class="allWrap ' + list[num] + ' allWrap' + (ii + 1) + '" id="allWrap' + (i + 1) + '"><span class="imageWrap"><img class="allImage" src="' + articleList[ii].mainImage + '" alt="' + list[num] + '"></span><span class="allTags"></span><span class="allTitle">' + articleList[ii].title + '</span>';
+                    console.log(createElement);
+                    i += 1;
                 }
             }
+
+            num = list.length - 1;
+
+            for (let i = 0; i < 4; i++) {
+                const element = document.querySelector("#newWrap" + (i + 1));
+                const createElement = '<span class="imageWrap"><img class="newImage" src="' + articleList[num].mainImage + '" alt="' + list[num] + '"></span><span class="newTitle">' + articleList[num].title + '</span><span class="newTags"></span><span class="newBody">' + articleList[num].content.substring(0, 80) + '…</span>';
+                element.innerHTML = createElement;
+
+                for (let ii = 0; ii < articleList[num].tag.length; ii++) {
+                    const elementTag = document.querySelector("#newWrap" + (i + 1) + " > .newTags");
+                    const createElementTag = '<span class="tag">#' + articleList[num].tag[ii] + '</span>';
+                    elementTag.insertAdjacentHTML("beforeend", createElementTag);
+                }
+
+                element.setAttribute("class", "newWrap" + " " + list[num]);
+
+                document.querySelector("#newWrap" + (i + 1) + "> .imageWrap").addEventListener('click', function () {
+                    alt = document.querySelector("#newWrap" + (i + 1) + "> .imageWrap > img").getAttribute('alt');
+                    sessionStorage.setItem("value", alt);
+                    window.location.href = "./article.html";
+                });
+                document.querySelector("#newWrap" + (i + 1) + "> .newTitle").addEventListener('click', function () {
+                    alt = document.querySelector("#newWrap" + (i + 1) + "> .imageWrap > img").getAttribute('alt');
+                    sessionStorage.setItem("value", alt);
+                    window.location.href = "./article.html";
+                });
+                document.querySelector("#newWrap" + (i + 1) + "> .newBody").addEventListener('click', function () {
+                    alt = document.querySelector("#newWrap" + (i + 1) + "> .imageWrap > img").getAttribute('alt');
+                    sessionStorage.setItem("value", alt);
+                    window.location.href = "./article.html";
+                });
+
+                num -= 1;
+
+            };
         });
     })
     .catch(err => console.error(err));
